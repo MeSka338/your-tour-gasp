ca5ccdbd9c7e2faa6bd3f1b25d873168d2d8bd52
MeSka338 <halk49114@gmail.com>
2023-12-04 23:08:49 +0300

    Add CustomCursor: cursor animation. #14

--- src/components/common/CustomCursor/CustomCursor.jsx
+++ src/components/common/CustomCursor/CustomCursor.jsx
@@ -2,42 +2,7 @@ import React, { useEffect, useState, useRef } from "react";
 import s from "./CustomCursor.module.scss";
 import gsap from "gsap";
 
-const CustomCursor = () => {
-  const rootRef = useRef();
-
-  const scaleAnim = gsap.timeline({ paused: true });
-
-  const cursoreMove = (e) => {
-    gsap.to(rootRef.current, {
-      x: e.clientX,
-      y: e.clientY,
-      ease: "power1.out",
-
-      stagger: {
-        each: 0.1,
-      },
-    });
-  };
-
-  const scaleAnimation = () => {
-    const targets = gsap.utils.toArray("a");
-
-    scaleAnim.to(rootRef.current, {
-      scale: 7,
-      duration: 0.2,
-    });
-
-    targets.forEach((target) => {
-      target.addEventListener("mouseenter", (e) => {
-        scaleAnim.play();
-      });
-
-      target.addEventListener("mouseleave", (e) => {
-        scaleAnim.reverse();
-      });
-    });
-  };
-
+const CustomCursor = ({ rootRef, cursoreMove, scaleAnimation }) => {
   useEffect(() => {
     gsap.set(rootRef.current, {
       xPercent: -50,
